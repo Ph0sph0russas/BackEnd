083e3a84ffa1082513d03bc15e534009182eb668
Ph0sph0russas <danila.ustiuzhanin@gmail.com>
2026-03-18 12:50:40 +0800

    Сделано задание 8, недоделан include

--- public/index.php
+++ public/index.php
@@ -1,53 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-</head>
-<body>
-    <nav class="navbar navbar-expand-lg bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Главная</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/chaos_emeralds">Изумруды хаоса</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/magic_carpet"/>Магический ковёр</a>
-        </li>
-        <!-- <li class="nav-item">
-          <a class="nav-link disabled">Disabled</a>
-        </li> -->
-      </ul>
-    </div>
-  </div>
-</nav>
-<div class="container">
+
     <?php
+
+    require_once '../vendor/autoload.php';
+    require_once "../controllers/MainController.php";
+    require_once "../controllers/ChaosEmeraldsController.php";
+    require_once "../controllers/ChaosEmeraldsImageController.php";
+    require_once "../controllers/ChaosEmeraldsInfoController.php";
+    require_once "../controllers/MagicCarpetController.php";
+    require_once "../controllers/MagicCarpetImageController.php";
+    require_once "../controllers/MagicCarpetInfoController.php";
+    require_once "../controllers/Controller404.php";
+    $loader = new \Twig\Loader\FilesystemLoader('../views');
+    $twig = new \Twig\Environment($loader);
+
+    $title = "";
+    $description = "";
+    $template = "";
+
+    $context = [];
+    $controller = new Controller404($twig);
     $url = $_SERVER["REQUEST_URI"];
-    if ($url=="/")
-        {
-            require "../views/main.php";
-        }
-    elseif ($url == "/chaos_emeralds")
-        {
-            require "../views/chaos_emeralds.php";
-        }
-    elseif ($url=="/magic_carpet")
-        {
-            require "../views/magic_carpet.php";
-        }
-    ?>
-</div>
-</body>
-</html>
+    if ($url == "/") {
+        $controller = new MainController($twig);
+    } elseif (preg_match("#^/chaos_emeralds/image#", $url)) {
+        $controller = new ChaosEmeraldsImageController($twig);
+    } elseif (preg_match("#^/chaos_emeralds/info#", $url)) {
+        $controller = new ChaosEmeraldsInfoController($twig);
+    } elseif (preg_match("#^/chaos_emeralds#", $url)) {
+        $controller = new ChaosEmeraldsController($twig);
+    } elseif (preg_match("#^/magic_carpet/image#", $url)) {
+        $controller = new MagicCarpetImageController($twig);
+    } elseif (preg_match("#^/magic_carpet/info#", $url)) {
+        $controller = new MagicCarpetInfoController($twig);
+    } elseif (preg_match("#^/magic_carpet#", $url)) {
+        $controller = new MagicCarpetController($twig);
+    }
+
+    if ($controller) {
+        $controller->get();
+    }
